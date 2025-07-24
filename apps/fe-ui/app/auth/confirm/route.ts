2ec0971508b4440c9ee0e801a80b4e3f5b1f79e2
xsahxl <xsahxl@126.com>
2025-07-24 10:41:57 +0800

    Update token retrieval logic in confirm route to support multiple query parameters
    - Modified the GET request handler to retrieve the token from either 'token_hash' or 'token' query parameters, enhancing flexibility in token validation.

--- apps/fe-ui/app/auth/confirm/route.ts
+++ apps/fe-ui/app/auth/confirm/route.ts
@@ -5,7 +5,7 @@ import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
-  const token_hash = searchParams.get("token_hash");
+  const token_hash = searchParams.get("token_hash") || searchParams.get("token");
   const type = searchParams.get("type") as EmailOtpType | null;
   const next = searchParams.get("next") ?? "/";
 
