ee224b0d2a55f66caf279c4fdcc006bd4e5d6960
xsahxl <xsahxl@126.com>
2025-07-24 11:26:44 +0800

    Enhance token verification and session exchange in confirm route
    - Updated the GET request handler to log the request URL for debugging purposes.
    - Refined token retrieval logic to exclusively use 'token_hash' and added support for 'code' parameter to exchange for a session, improving authentication flow.

--- apps/fe-ui/app/auth/confirm/route.ts
+++ apps/fe-ui/app/auth/confirm/route.ts
@@ -5,13 +5,14 @@ import { type NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
-  const token_hash = searchParams.get("token_hash") || searchParams.get("token");
+  console.log("request.url", request.url);
+  const token_hash = searchParams.get("token_hash");
   const type = searchParams.get("type") as EmailOtpType | null;
   const next = searchParams.get("next") ?? "/";
+  const code = searchParams.get("code");
+  const supabase = await createClient();
 
   if (token_hash && type) {
-    const supabase = await createClient();
-
     const { error } = await supabase.auth.verifyOtp({
       type,
       token_hash,
@@ -21,6 +22,14 @@ export async function GET(request: NextRequest) {
       redirect(next);
     } else {
       // redirect the user to an error page with some instructions
+      redirect(`/auth/error?error=${error?.message}`);
+    }
+  }
+  if (code) {
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      redirect(next);
+    } else {
       redirect(`/auth/error?error=${error?.message}`);
     }
   }
